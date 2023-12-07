94a91e62d3a04b4dad023926506c9c652cb9fbda
Jiyun Yang <jiyuunyang@gmail.com>
2023-12-07 16:01:07 +0900

    [Chore] Add comments

--- lib/models/webtoon.dart
+++ lib/models/webtoon.dart
@@ -2,6 +2,7 @@
 class WebtoonModel {
   final String title, thumb, id;
 
+// Map : key-value structure
   WebtoonModel.fromJson(Map<String, dynamic> json)
       : title = json['title'],
         thumb = json['thumb'],
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -7,6 +7,7 @@ class ApiService {
   final String baseUrl = 'https://webtoon-crawler.nomadcoders.workers.dev';
   final String today = 'today';
 
+// async 함수는 반환값이 항상 Future
   Future<List<WebtoonModel>> getTodaysToons() async {
     final url = Uri.parse('$baseUrl/$today');
     // 이 부분이 응답 받을 때까지 기다리라 (await)
@@ -14,9 +15,11 @@ class ApiService {
     final response = await http.get(url);
     // 응답 상태가 성공일 경우
     if (response.statusCode == 200) {
+      //json 으로 바꿔줌 : josonDecode
       final List<dynamic> webtoons = jsonDecode(response.body);
       for (var webtoon in webtoons) {
-        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+        final instance = WebtoonModel.fromJson(webtoon);
+        webtoonInstances.add(instance);
       }
       return webtoonInstances;
     }
