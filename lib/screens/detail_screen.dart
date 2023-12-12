49d1556ce605de05f6242081d01f069580cac6f6
Jiyun Yang <jiyuunyang@gmail.com>
2023-12-12 11:30:48 +0900

    [Add] Add Detail Screen and Navigator

---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  final String title, thumb, id;
+  const DetailScreen({
+    super.key,
+    required this.title,
+    required this.thumb,
+    required this.id,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          elevation: 2,
+          foregroundColor: Colors.green,
+          backgroundColor: Colors.white,
+          title: Text(
+            title,
+            style: const TextStyle(fontSize: 24),
+          ),
+        ),
+        body: Column(
+          children: [
+            const SizedBox(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    width: 250,
+                    // clipbehavior : 자식이 부모 영역 침범시에 자름
+                    clipBehavior: Clip.hardEdge,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 15,
+                            offset: const Offset(10, 10),
+                            color: Colors.black.withOpacity(0.3),
+                          )
+                        ]),
+                    child: Image.network(thumb)),
+              ],
+            ),
+          ],
+        ));
+  }
+}
--- lib/widgets/webtoon_widget.dart
+++ lib/widgets/webtoon_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterwebtoon/screens/detail_screen.dart';
 
 class Webtoon extends StatelessWidget {
   final String title, thumb, id;
@@ -11,27 +12,45 @@ class Webtoon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Container(
-          width: 250,
-          // clipbehavior : 자식이 부모 영역 침범시에 자름
-          clipBehavior: Clip.hardEdge,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 15,
-                  offset: const Offset(10, 10),
-                  color: Colors.black.withOpacity(0.3),
-                )
-              ]),
-          child: Image.network(thumb)),
-      const SizedBox(height: 10),
-      Text(title,
-          style: const TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.w600,
-          )),
-    ]);
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailScreen(
+              title: title,
+              thumb: thumb,
+              id: id,
+            ),
+            // 이미지가 바닥에서부터 올라옴 (해당 설정 없을 경우 옆으로 네비게이션)
+            fullscreenDialog: true,
+          ),
+        );
+      },
+      child: Column(
+        children: [
+          Container(
+              width: 250,
+              // clipbehavior : 자식이 부모 영역 침범시에 자름
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 15,
+                      offset: const Offset(10, 10),
+                      color: Colors.black.withOpacity(0.3),
+                    )
+                  ]),
+              child: Image.network(thumb)),
+          const SizedBox(height: 10),
+          Text(title,
+              style: const TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+              )),
+        ],
+      ),
+    );
   }
 }
